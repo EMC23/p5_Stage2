1cca8352dcdc900a32f82d1c63a1840de858ac7d
Lisa Corcoran <lcdemc@gmail.com>
2016-11-19 10:22:22 +0100

    add gradient shape to control with midi controller, wip

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ var countries = [],
     indCountry;
 
 var notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
-
+var rose;
+var stars;
 
 jQuery.get('countries.json', function(json) {
      countries = json.countries;
@@ -58,6 +59,10 @@ p5.midi.onInput = function(event) {
       countries[1] = country;
       console.log('2');
     }*/
+
+    changeColor();
+
+
 }
 function printMidi(country,indCountry) {
 
@@ -88,11 +93,77 @@ function printMidi(country,indCountry) {
 
 }
 
+function preload()
+{
+    // load image
+    rose = loadImage("images/rose.png");
+    stars = loadImage("images/star-bg.jpg");
+}
 
 function setup(indCountry) {
     createCanvas(640, 480);
     textFont('arial', 64);
     textAlign(LEFT);
+
+    // hide mouse cursor
+    noCursor();
+
+
+    test = createImage(230, 230);
+    test.loadPixels();
+
+    var r = 20;
+    var g = 40;
+    var b = 156;
+
+
+    console.log(r,g,b,a);
+
+    for(var x = 0; x < test.width; x++) {
+        for(var y = 0; y < test.height; y++) {
+            var a = map(y, 0, test.height, 255, 0);
+            test.set(x, y, [r, g, b, a]);
+        }
+    }
+
+    test.updatePixels();
+
+
+}
+
+function changeColor() {
+
+
+
+}
+
+
+
+
+function draw() {
+    //background(255);
+
+    // display background image
+    //image(stars, 0, 0);
+    //image(rose,mouseX,mouseY); // draws the image at the specified x and y location
+    //image(rose,canvas.width/2-rose.width/2,canvas.height/2-rose.height/2); // draws the image at the specified x and y location
+
+
+
+
+
+
+
+
+
+    // replace this with midi control
+    //image(test, mouseX-test.width/2, mouseY-test.height/2);
+    image(test, 130, 180);
+
+
+
+
+
 }
 
 function mousePressed(){
@@ -115,3 +186,5 @@ function mouseClicked() {
         x = y = 50;
     }
 }
+
+
